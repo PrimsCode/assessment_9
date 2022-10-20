8beea9767cfb432b5fd689422537b7831806e725
Prima <primabreuerjenkins@gmail.com>
2022-10-20 09:22:48 -0500

    solved unroll problem

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -3,18 +3,29 @@ function unroll(squareArray) {
 
     while(squareArray.length > 0){
         //get top row
-        unrollArray = [...unrollArray, squareArray.shift()];
+        if (squareArray.length !== 0){
+            unrollArray = [...unrollArray, squareArray.shift()];
+        }        
+
         //get right coloumn
-        unrollArray = [...unrollArray, squareArray.map(row => row.pop())];
+        if (squareArray.length !== 0){
+            unrollArray = [...unrollArray, squareArray.map(row => row.pop())];
+        }
 
         //get bottom row in reverse
-        unrollArray = [...unrollArray, squareArray.pop().reverse()];
+        if (squareArray.length !== 0){
+            unrollArray = [...unrollArray, squareArray.pop().reverse()];
+        }
 
         //get left column
-        unrollArray = [...unrollArray, squareArray.map(row => row.shift()).reverse()];
+        if (squareArray.length !== 0){
+            unrollArray = [...unrollArray, squareArray.map(row => row.shift()).reverse()];
+        }
     }
     //returning an array with no sub elements
     return unrollArray.flat(1);
 }
+
+
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -7,14 +7,24 @@ const square = [
   [13, 14, 15, 16]
 ];
 
+const smallerSquare = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"]
+];
+
 describe("#unroll", function () {
 
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
 
-  test('turning time string into a word string', () => {
+  test('unrolling a sqare', () => {
     expect(unroll(square)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
+  })
+
+  test('unrolling small sqare', () => {
+    expect(unroll(smallerSquare)).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"]);
   })
 
 });
